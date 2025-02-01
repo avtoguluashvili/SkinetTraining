73488c84b0dffcd3c73956a44086782092567c03
avto.guluashvili <avto.guluashvili@gmail.com>
2025-02-01 18:37:14 +0400

    added github final version

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,5 +1,7 @@
-﻿using API.DTOs;
+﻿using System.Security.Claims;
+using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -34,5 +36,33 @@ public class BuggyController : BaseApiController
     public IActionResult GetValidationError(CreateProductDto product)
     {
         return Ok();
+    }
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok("Hello " + name + " with the id of " + id);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("admin-secret")]
+    public IActionResult GetAdminSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var isAdmin = User.IsInRole("Admin");
+        var roles = User.FindFirstValue(ClaimTypes.Role);
+
+        return Ok(new 
+        {
+            name,
+            id,
+            isAdmin,
+            roles
+        });
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,19 +1,23 @@
 ﻿using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-public class StoreContext : DbContext
+public class StoreContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
 {
-    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
-    {
-    }
-
     public DbSet<Product> Products { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreContext).Assembly);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
     }
 }
